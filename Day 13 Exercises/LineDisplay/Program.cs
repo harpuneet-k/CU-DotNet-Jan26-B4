32e159085d83e52c933bfa108b05bd9f3141823a
HARPUNEET <puneetsandhu1530@gmail.com>
2026-01-21 16:16:26 +0530

    Day 13 Exercises added

---
+++ Day 13 Exercises/LineDisplay/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LineDisplay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            LinePrint();
+            LinePrint('+');
+            LinePrint('$', 60);
+        }
+
+        static void LinePrint(char ch = '-', int count = 40)
+        {
+            Console.WriteLine($"\n Printing pattern with:{ch}");
+            for (int i = 0; i < count; i++)
+            {
+
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+    }
+}
